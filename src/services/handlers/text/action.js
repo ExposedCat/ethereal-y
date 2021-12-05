d4d7831af1f2371fe44f0fa94cba5b2a6ba8bde2
Artem Prokop <artem13.prokop@gmail.com>
2021-12-05 23:41:15 +0200

    Improve roleplay command

--- src/services/handlers/text/action.js
+++ src/services/handlers/text/action.js
@@ -2,19 +2,22 @@ import { texts } from '../../../static/texts.js'
 import { buttons } from '../../../static/buttons.js'
 
 
+const replacements = [
+    ['йти', 'шел(-ла)'],
+    ['реть', 'р(-ла)'],
+    ['ться', 'лся(-ась)'],
+    ['хнуть', 'х(-ла)'],
+    ['ть', 'л(-а)'],
+]
+
 async function action(text) {
     let result = text
-    const replacements = [
-        ['йти', 'шел(-ла)'],
-        ['еть', 'л(-а)'],
-        ['ться', 'лся(-ась)'],
-        ['ть', 'л(-а)'],
-    ]
-    const ending = `(?=[^А-ЯЁа-яё]|$)`
+    const ending = `(?=[^А-ЯЁа-яё*]|$)`
     for (const [from, to] of replacements) {
         const regexp = new RegExp(from + ending, 'gi')
         result = result.replace(regexp, to)
     }
+    result = result.replace(/(?<=[А-ЯЁа-яё])\*(?=\s|\n|$)/g, '')
     return result
 }
 
