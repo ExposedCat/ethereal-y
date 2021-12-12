f4c223f1194b563d1453cc6ea08bf975a02ed651
Artem Prokop <artem13.prokop@gmail.com>
2021-12-12 14:33:01 +0200

    Update texts

--- src/static/texts.js
+++ src/static/texts.js
@@ -1,29 +1,25 @@
 const texts = {
     other: {
-        greeting: 'Hello!',
+        greeting: '👋 Hello!',
         hints: {
             cron: '<code>/cron * * * * * Pet a loli</code>',
             reminder: '<code>/reminder 31.01 12:00 Pet a loli</code>\n<code>/reminder yesterday 12:00 Pet a loli</code>'
         },
-        notification: text => `Remainder: «${text}»`
+        notification: text => `🕓 Remainder: «${text}»`
     },
     input: {
 
     },
     success: {
-        groupAuthorized: title => `Group "${title}" authorized`,
-        reminderSet: (date, time) => `You will be notified at ${date} ${time}`,
-        cronSet: (cron, next) => `You will be notified by time rule: ${cron}\nNext notification at: ${next}`
+        reminderSet: (date, time) => `✍️ You will be notified at ${date} ${time}`,
+        cronSet: (cron, next) => `✍️ You will be notified by time rule: <code>${cron}</code>\nNext notification at: ${next}`
     },
     errors: {
-        unknownCommand: 'Unknown command',
-        invalidArguments: command => `Invalid arguments\n\nSyntax: ${texts.other.hints[command]}`,
-        invalidSyntax: 'Invalid syntax',
-        noReply: 'Reply message is not specified',
-        regexpError: error => `RegExp error: ${error}`
-    },
-    buttons: {
-        settings: 'Settings'
+        unknownCommand: '🤔 Unknown command',
+        invalidArguments: command => `🤯 Invalid arguments\n\nSyntax: ${texts.other.hints[command]}`,
+        invalidSyntax: '🤯 Invalid syntax',
+        noReply: '🤔 Reply message is not specified',
+        regexpError: error => `🤬 RegExp error: ${error}`
     }
 }
 
