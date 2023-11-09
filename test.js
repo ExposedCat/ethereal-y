812c7c8bfb67e01b5b5b6df59ec612ab956f4fdb
Artem Prokop <artem13.prokop@gmail.com>
2023-11-09 17:13:03 +0100

    Cleanup

--- test.js
+++
@@ -1,8 +0,0 @@
-import {matches} from 'super-regex'
-
-const parser = new RegExp(/(?:([gim]{0,3}))?\/(.+)\/((?:.|)+?)\/$/gm)
-const matched = matches(parser,  `im/test/y/
-/test2/s/`);
-for (const match of matched) {
-  console.log(match)
-}
